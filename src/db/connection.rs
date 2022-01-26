3bf2e4f2679eb4c5f1bf34269c8a27ac2ef0c79c
joao <joao.lobom@gmail.com>
2022-01-26 21:06:33 +0000

    Add further dependencies

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -1,17 +1,18 @@
 #[macro_use]
-extern crate diesel;
-extern crate dotenv;
+pub mod db_connection {
+    extern crate diesel;
+    extern crate dotenv;
 
-use diesel::prelude::*;
-use diesel::pg::PgConnection;
-use dotenv::dotenv;
-use std::env;
+    use diesel::pg::PgConnection;
+    use diesel::prelude::*;
+    use dotenv::dotenv;
+    use std::env;
 
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
+    pub fn establish_connection() -> PgConnection {
+        dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        PgConnection::establish(&database_url)
+            .expect(&format!("Error connecting to {}", database_url))
+    }
 }
